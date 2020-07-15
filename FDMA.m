336427b5f8664f7e98984359b8d9bbaf996431e5
Ben <benmo009@gmail.com>
2020-07-15 19:52:26 -0400

    update FDMA function

--- FDMA.m
+++ FDMA.m
@@ -1,9 +1,11 @@
-% Function for running the FDMA simulation script multiple times more easily.
-% Input the total simulation time, number of sources, average rate of packet
-% arrivals (lambda), and average service time (mu). Outputs the average age over
-% the duration of the simulation
+% Function for running the FDMA simulation multiple times more easily. Input the
+% total simulation time, number of sources, average rate of packet arrivals
+% (lambda), and average service rate (mu). Lambda should be in the form of a
+% vector thats length matches the number of soures, and mu should a double.
+% Outputs the average age over the duration of the simulation and its standard
+% deviation
 
-function avgAge = FDMA(tFinal, numSources, lambda, mu, )
-    
+function [avgAge, stdDevAge] = FDMA(tFinal, numSources, lambda, mu)
+
 
 end
