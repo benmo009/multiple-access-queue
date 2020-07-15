95c87058dd8a80b18fb2b227e72efcfb206d6f9d
Ben <benmo009@gmail.com>
2020-07-15 01:57:19 -0400

    added TDMA function

---
+++ TDMA.m
@@ -0,0 +1,10 @@
+% Function for running the TDMA simulation multiple times more easily. Input the
+% total simulation time, number of sources, slot duration, average rate of
+% packet arrivals (lambda), and average service rate (mu). Outputs the average
+% age over the duration of the simulation and its standard deviation
+
+function [avgAge, stdDevAge] = TDMA(tFinal, numSources, slotDuration, lambda, mu)
+
+
+
+end
