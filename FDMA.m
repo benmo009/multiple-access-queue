c83c7ecd54ba17e4d952c0667f97e7005402e1d5
Ben <benmo009@gmail.com>
2020-07-16 11:11:14 -0400

    added working FDMA function

--- FDMA.m
+++ FDMA.m
@@ -1,11 +1,23 @@
+% FDMA.m
 % Function for running the FDMA simulation multiple times more easily. Input the
-% total simulation time, number of sources, average rate of packet arrivals
-% (lambda), and average service rate (mu). Lambda should be in the form of a
-% vector thats length matches the number of soures, and mu should a double.
-% Outputs the average age over the duration of the simulation and its standard
-% deviation
+% total simulation time, timestep, number of sources, average rate of packet
+% arrivals (lambda), and average service rate (mu). Lambda and mu should be in
+% the form of a vector thats length matches the number of soures. Outputs the
+% average age over the duration of the simulation and the average wait time in
+% the queue
 
-function [avgAge, stdDevAge] = FDMA(tFinal, numSources, lambda, mu)
+function [avgAge, avgWait] = FDMA(tFinal, dt, numSources, lambda, mu, plotResult)
+    % Set plotResult to false if it wasn't given
+    if nargin == 5
+        plotResult = false;
+    end
 
+    % Initialize vectors for storing average age and wait times
+    avgAge = zeros(numSources, 1);
+    avgWait = zeros(numSources, 1);
 
+    % Simulate the queue for each source
+    for i = 1:numSources
+        [avgAge(i), avgWait(i)] = SimulateAoI(tFinal, dt, lambda(i), mu(i), plotResult, i);
+    end
 end
