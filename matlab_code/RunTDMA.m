436f22724882a32ab1421f044603bf9ced6cb6ab
Ben <benmo009@gmail.com>
2020-07-24 18:25:30 -0400

    set queueSize to Inf

--- matlab_code/RunTDMA.m
+++ matlab_code/RunTDMA.m
@@ -1,7 +1,7 @@
 % RunTDMA.m
 % Simulate the TDMA queue multiple times to collect data
 
-% clc 
+clc 
 close all
 
 % Set simulation parameters
@@ -30,9 +30,9 @@ slotDuration = round(slotDuration / dt) * dt; % round to same order as time step
 
 priority = [0, 0]; % Both sources have same priority
 
-queueSize = 3;
+queueSize = Inf;
 
-numSimulations = 100;
+numSimulations = 1000;
 avgAge = zeros(numSources, numSimulations);
 avgWait = zeros(numSources, numSimulations);
 
@@ -49,19 +49,34 @@ totalAvgWait = sum(avgWait, 2)/numSimulations;
 stdDevWait = std(avgWait, 0, 2);
 
 fprintf('\nRan %d simulations for TDMA with %d sources.\n', numSimulations, numSources);
+fprintf('Simulation time was %d seconds with step size %s\n', tFinal, strtrim(num2str(dt)));
 fprintf('Time slot P = %.1f (%.1f s)\n', probability, slotDuration);
-fprintf('Maximum Queue Size: %d', queueSize);
-fprintf('lambda:\n');
+fprintf('Maximum Queue Size: %d\n', queueSize);
+
+fprintf('\nlambda:\n');
 for i = 1:numSources
     fprintf('\tSource %d: %s\n', i, strtrim(rats(lambda(i))));
 end
+
 fprintf('mu: %s\n', strtrim(rats(mu)));
+
 fprintf('\nDATA\n');
 fprintf('Average Age:\n');
 for i = 1:numSources
-    fprintf('\t Source %d: %.2f, std dev = %.2f\n', i, totalAvgAge(i), stdDevAge(i));
+    fprintf('\tSource %d: %.2f, std dev = %.2f\n', i, totalAvgAge(i), stdDevAge(i));
 end
 fprintf('Average Delay:\n');
 for i = 1:numSources
-    fprintf('\t Source %d: %.2f, std dev = %.2f\n', i, totalAvgWait(i), stdDevWait(i));
+    fprintf('\tSource %d: %.2f, std dev = %.2f\n', i, totalAvgWait(i), stdDevWait(i));
+end
+
+for i = 1:numSources
+    subplot(numSources, 1, i)
+    plot([1:numSimulations], avgAge(i, :), '.');
+    hold on
+    plot([1:numSimulations], totalAvgAge(i) .* ones(1, numSimulations))
+    legend('Average Age for each simulation', sprintf('Overall Average Age = %.2f', totalAvgAge(i)));
+    xlabel('Simulation Number');
+    ylabel('Average Age (s)')
+    title(sprintf('TDMA %d simulations, source %d',numSimulations, i))
 end
