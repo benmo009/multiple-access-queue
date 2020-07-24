56ce21a8a546605a18d7235b59c61635d978aeea
Ben <benmo009@gmail.com>
2020-07-24 02:28:52 -0400

    added comments

--- matlab_code/AoILimitQueue.m
+++ matlab_code/AoILimitQueue.m
@@ -45,21 +45,28 @@ function [avgAge, avgWait, served] = AoILimitQueue(tFinal, dt, lambda, mu, queue
     currentTime = min(toServe(1), lastPacketServed);
     packetsServed = 1;
     
+    % Initialize age vector
     age = t;
 
+    % Loop through important times such as when a packet arrives or when a
+    % packet just finished being served
     while (true)
         if currentTime > lastPacketServed 
+            % A packet arrived after the previous packet was served. The server
+            % was idle and there are no packets in the queue. Immediately serve
+            % this packet
             packetsServed = packetsServed + 1;
             timeReceived(packetsServed) = currentTime + S(packetsServed) + W(packetsServed);
-            lastPacketServed = timeReceived(packetsServed);
+            % Record when this packet is done
+            lastPacketServed = timeReceived(packetsServed); 
 
+            % Set the next time stamp to go to
             if isempty(toServe)
                 currentTime = lastPacketServed;
             else
                 currentTime = min(toServe(1), lastPacketServed);
             end
-            
-
+        
         elseif currentTime == lastPacketServed
             % Packet just finished being served. Take next packet from the queue
             % If the queue is empty, skip to when the next packet arrives
@@ -69,34 +76,38 @@ function [avgAge, avgWait, served] = AoILimitQueue(tFinal, dt, lambda, mu, queue
             ageIndex = uint32(currentTime/dt + 1);
 
             if ageIndex > length(age)
-                % Double the age and time vectors
+                % Double the age and time vectors if needed
                 tFinal = 2 * tFinal;
                 t = [0:dt:tFinal];
                 ageEnd = age(:, end) + dt;
                 ageAppend = ageEnd + [0:dt:tFinal / 2 - dt];
                 age = [age, ageAppend];
             end
-
+            % Reduce the remaining age
             reduceAge = age(ageIndex) - packetAge;
             age(ageIndex:end) = age(ageIndex:end) - reduceAge;
 
+            % Take next packet from the queue
             if queue.size() == 0
                 % queue is empty
                 if isempty(toServe)
                     break;
                 end
+                % Set next timestamp to when next packet arrives
                 currentTime = toServe(1);
                 packet = currentTime;
                 toServe(1) = [];
 
             else
+                % Take the next packet from the queue
                 packet = queue.remove();
                 packetsServed = packetsServed + 1;
                 W(packetsServed) = currentTime - packet;
                 timeReceived(packetsServed) = currentTime + S(packetsServed);
-
+                % Record when this packet finished being served
                 lastPacketServed = timeReceived(packetsServed);
 
+                % Set the net time stamp
                 if isempty(toServe)
                     currentTime = lastPacketServed;
                 else
@@ -105,17 +116,17 @@ function [avgAge, avgWait, served] = AoILimitQueue(tFinal, dt, lambda, mu, queue
                 
             end
 
-
         elseif currentTime < lastPacketServed
             % Newest packet arrived when the server is still working on a packet
             % Add newly arrived packet to queue
-
             queue.add(currentTime);
+            % Remove oldest packet if queue size is exceeded
             if queue.size() > queueSize
                 queue.remove();
             end
-
             toServe(1) = [];
+
+            % Set the next time stamp
             if isempty(toServe)
                 currentTime = lastPacketServed;
             else
