0aa7f1927b722e4436e5248be94ce802f543d8a7
Ben <benmo009@gmail.com>
2020-07-26 17:13:16 -0400

    added fix for when no packets are sent

--- matlab_code/GenerateTransmissions.m
+++ matlab_code/GenerateTransmissions.m
@@ -1,22 +1,26 @@
 function event = GenerateTransmissions(lambda, t, plot_result)
-    if(nargin==2)
+
+    if (nargin == 2)
         plot_result = false;
     end
-    
+
     % Get the step size and number of steps
     N = length(t);
     dt = t(2) - t(1);
-    
+
     % Generate vector of when transmission times occured
-    event = zeros(1, N);  % array of when transmissions occur
-    R = rand(size(event));  % generate array of random numbers
-    event(R<lambda*dt) = 1; % event occurs every time R<lambda*delta
-    
-    if(plot_result)
+    event = zeros(1, N); % array of when transmissions occur
+    R = rand(size(event)); % generate array of random numbers
+    event(R < lambda * dt) = 1; % event occurs every time R<lambda*delta
+
+    % Keep generating events until at least one happens
+    if sum(event) == 0
+        event = GenerateTransmissions(lambda, t, plot_result);
+    end
+
+    if (plot_result)
         stem(t, event);
         hold on
     end
+
 end
-
-
-
