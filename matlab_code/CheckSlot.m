947fde6f38bdaf64fd7baaabd7b3ddceff6067dc
Ben <benmo009@gmail.com>
2020-08-13 17:08:09 -0400

    sources can now have different slot durations

--- matlab_code/CheckSlot.m
+++ matlab_code/CheckSlot.m
@@ -2,48 +2,41 @@
 % is was sent based on the slot duration. The function will return true if
 % the packet's source is recieved during a slot that matches the source.
 
-function [serveSource, slotNumber] = CheckSlot(time, numSources, slotDuration, priority, timeTransmit)
+function [serveSource, slotNumber, slotTransition] = CheckSlot(time, numSources, slotDuration, priority, timeTransmit)
 
-    idx = timeTransmit(2,:)==time;
-    currentSource = timeTransmit(1, idx);
-    
-    slotNumber = fix(time/slotDuration);
+
+    totalSlot = sum(slotDuration);
+    slotNumber = fix(time/totalSlot);
+    relativeTime = mod(time, totalSlot);
+
+    for i = 1:length(slotDuration)
+        relativeTime = relativeTime - slotDuration(i);
+        if relativeTime < 0
+            break;
+        end
+    end
+
+    serveSource = i;
 
     % Added to remove floating point error bug where fix would return an
     % incorrect value
-    difference = (time/slotDuration) - slotNumber;
+    difference = (time/totalSlot) - slotNumber;
     if abs(difference - 1) < 1e-9
         slotNumber = slotNumber + 1;
     end
+
+    timeToNextSlot = -relativeTime; % It should be a negative number
+    slotTransition = time + timeToNextSlot;
     
-    serveSource = mod(slotNumber, numSources) + 1;  % is either a 1 or 2
-    
-    if serveSource ~= currentSource
-        if priority(currentSource) > priority(serveSource)
-            fprintf("At time: %d, priority take over serveSource slot!\n", time)
-            serveSource = currentSource;
+    % Priority
+    idx = find(timeTransmit(2,:) == time);
+    if ~isempty(idx)
+        currentSource = timeTransmit(1,idx);
+        if serveSource ~= currentSource
+            if priority(currentSource) > priority(serveSource)
+                fprintf("At time: %d, priority take over serveSource slot!\n", time)
+                serveSource = currentSource;
+            end
         end
     end
 end
-
-% this_packet_source = packet(1);
-% this_packet_timestamp = packet(2);
-% this_packet_priority = 1
-% 
-% priority_array = zeros(numSources,1);
-% priority_array(1) = 1;
-% priority_array(2) = 0;
-% 
-% slotNumber = fix(timestamp/slotDuration);
-% serverSource = mod(slotNumber, numSources)+1;
-% 
-% % if everything match
-% serve = (this_packet_source == serverSource);
-% % now take care the case of priority
-% if ~serve:
-%     % if the higher priority packet land in slot belong to 
-%     % low priority packet
-%     if priority_array(this_packet_priority) > priority_array(serverSource):
-%         serve = True;
-%     end
-% end
