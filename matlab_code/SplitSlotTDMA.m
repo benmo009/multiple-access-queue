ecac98cf1d3ba461265aecbf847247e852cf2739
Ben <benmo009@gmail.com>
2020-08-14 13:52:26 -0400

    wrote simulation script

--- matlab_code/SplitSlotTDMA.m
+++ matlab_code/SplitSlotTDMA.m
@@ -26,7 +26,7 @@ mu = 1/30;
 
 % Slot duration
 T = 1/mu;
-b = linspace(0.05, 0.95, 200);  % Splitting factor
+b = linspace(0.05, 0.95, 100);  % Splitting factor
 
 % Infinite queue
 queueSize = Inf;
@@ -37,3 +37,41 @@ avgWait = zeros(numSources, size(b, 2));
 
 simAvgAge = zeros(numSources, numSimulations);
 simAvgWait = zeros(numSources, numSimulations);
+
+tic
+slotDuration = zeros(numSources, 1);
+for i = 1:length(b)
+    slotDuration(1) = b(i) * T;
+    slotDuration(2) = (1 - b(i)) * T;
+    for j = 1:numSimulations
+        [simAvgAge(:,j), simAvgWait(:,j)] = TDMA(tFinal, dt, numSources, slotDuration, lambda, mu);
+    end
+    avgAge(:,i) = sum(simAvgAge,2) ./ numSimulations;
+    avgWait(:,i) = sum(simAvgWait,2) ./ numSimulations;
+end
+toc
+
+figure
+plot(b, avgAge(1,:), '.')
+hold on
+plot(b, avgAge(2,:), '.')
+legend("Source 1", "Source 2")
+
+xlabel('Slot splitting factor, b');
+ylabel('Average Age (s)');
+
+annotationStr = ['\mu = ', strtrim(rats(1/30))];
+annotationStr = [annotationStr, ', Slot (T) = 1/\mu'];
+annotationStr = {annotationStr, 'T_1 = b * T', 'T_2 = (1 - b) * T'};
+annotation('textbox', [0.15 0.7, 0.2, 0.2], 'String', annotationStr, 'FitBoxToText', 'on', 'BackgroundColor', 'w')
+
+figure
+plot(b, avgWait(1,:), '.')
+hold on
+plot(b, avgWait(2,:), '.')
+legend("Source 1", "Source 2")
+
+xlabel("Slot Splitting Factor, b")
+ylabel("Average Delay (s)")
+
+annotation('textbox', [0.15 0.7, 0.2, 0.2], 'String', annotationStr, 'FitBoxToText', 'on', 'BackgroundColor', 'w')
