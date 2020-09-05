68c138d12bc33c1b986e955d167bd0158b79fa95
Ben <benmo009@gmail.com>
2020-09-05 13:00:28 -0400

    Changed outpuf file from .txt to .csv

--- C++/SimulateAoI.cpp
+++ C++/SimulateAoI.cpp
@@ -1,18 +1,19 @@
 #include "AoIQueue.h"
 
-int main() {
+int main(int argc, char* argv[]) {
 
-    double tFinal = 360;
+    double tFinal = 5040;
     double tStep = 0.1;
     double lambda = 1.0/60.0;
     double mu = 0.1;
 
     AoIQueue queue(tFinal, tStep, lambda, mu);
+    queue.print();
 
     std::cout << "Average Age: " << queue.getAvgAge() << std::endl;
     std::cout << "Average Delay: " << queue.getAvgDelay() << std::endl;
 
-    std::string filename = "AgeOutput.txt";
+    std::string filename = "AgeOutput.csv";
     queue.exportAge(filename);
 
     return 0;
