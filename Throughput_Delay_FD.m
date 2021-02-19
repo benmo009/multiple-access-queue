85e7c049bb6c5c11ba2f3a9a741ecd3ad6a21fdb
aiden <cheny51@rpi.edu>
2021-02-19 15:55:13 -0500

    Throughput delay plots

--- Throughput_Delay_FD.m
+++ Throughput_Delay_FD.m
@@ -9,37 +9,45 @@
 % n = m * p * mu
 
 m = 1;
-mu = [1/30, 1/30];
-
+%mu = [1/30, 1/30];
+mu = 1/30;
 % arrival rate for both user
 %Need to make sure that the arrival rate will always be less than the service rate
-lambda = [1/80; 1/50];
-b=0.001;
-n = [0:b:1; flip(0:b:1)];
+lambda = [0.003; 0.003];
+bmin = min(lambda)/mu;
+bmax = 0.9;
+
+b=0.01;
+n = [bmin:b:bmax; flip(bmin:b:bmax)];
 % Delay
-D_each  = @(lambda,n, mu) 1./(mu-lambda.*n);
-D_total = @(lambda1, lambda2, n, mu) 1./(mu-(lambda1.*n + lambda2.*flip(n)));
+D_each  = @(lambda,n, mu) 1./(mu.*n-lambda);
 figure;
 subplot(2,1,1);
-plot(n(1,:), D_each(lambda(1),n(1,:),mu(1)),'r');
+semilogy(n(1,:), D_each(min(lambda),n(1,:),mu),'r');
 hold on
-plot(n(1,:), D_each(lambda(2),n(2,:),mu(2)),'blue');
+semilogy(n(1,:), D_each(max(lambda),n(2,:),mu),'blue');
 heading = ['Throughput ', 'vs. Individual Delay for User 1 and User 2'];
-key1 = ['$D_1(\lambda_1, \mu_1, b)=\frac{1}{\mu_1-b\lambda_1},\lambda_1=$'+string(lambda(1))+', $\mu_1 =$'+string(mu(1))];
-key2 = ['$D_2(\lambda_2, \mu_2, b)=\frac{1}{\mu_2-(1-b)\lambda_2},\lambda_2=$'+string(lambda(2))+', $\mu_2 =$'+string(mu(2))];
+key1 = ['$D_1(\lambda_1, \mu, b)=\frac{1}{b\mu-\lambda_1},\lambda_1=$'+string(min(lambda))+', $\mu =$'+string(mu)];
+key2 = ['$D_2(\lambda_2, \mu, b)=\frac{1}{(1-b)\mu-\lambda_2},\lambda_2=$'+string(max(lambda))+', $\mu =$'+string(mu)];
 legend([key1; key2], 'Interpreter','latex');
 title(heading,'Interpreter','latex')
+xlim([bmin bmax]);
+ylabel('Delay (D)','Interpreter','latex')
+xlabel('Split factor applied to service rate (b)', 'Interpreter','latex')
 subplot(2,1,2);
-DelayTotal = D_each(lambda(2),n(2,:),mu(2))+D_each(lambda(1),n(1,:),mu(1));
-plot(n(1,:), DelayTotal,'green');
+DelayTotal = D_each(min(lambda),n(1,:),mu)+D_each(max(lambda),n(2,:),mu);
+DelayTotal(DelayTotal<0)=nan;
+semilogy(n(1,:), DelayTotal,'green');
+xlim([bmin bmax]);
 [M,I]= min(DelayTotal);
 xline(n(1,I),'linewidth',2, 'color', 'blue');
 heading = ['Throughput ', 'vs. Sum Delay for All User'];
 title(heading, 'Interpreter','latex')
-
-data_first_d1 = gradient(D_each(lambda(1),n(1,:),mu(1))) ./ gradient(n(1,:));
-data_first_d2 = gradient(D_each(lambda(2),n(2,:),mu(2))) ./ gradient(n(2,:));
-data_first_d3 = gradient(DelayTotal) ./ gradient(n(1,:));
+ylabel('Delay (D1+D2)', 'Interpreter','latex')
+xlabel('Split factor applied to service rate (b)', 'Interpreter','latex')
+%data_first_d1 = gradient(D_each(lambda(1),n(1,:),mu(1))) ./ gradient(n(1,:));
+%data_first_d2 = gradient(D_each(lambda(2),n(2,:),mu(2))) ./ gradient(n(2,:));
+%data_first_d3 = gradient(DelayTotal) ./ gradient(n(1,:));
 % figure;
 % plot(n(1,:), data_first_d1,'r');
 % hold on
